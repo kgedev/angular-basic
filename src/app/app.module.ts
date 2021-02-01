5787d49b8536003b220d6d5349ee28f51121b715
s8-dev <g.kuznetsov@siams.com>
2021-02-01 12:54:49 +0500

    add custom components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {PostComponent} from './post/post.component';
+import { Post2Component } from './post2/post2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    Post2Component
   ],
   imports: [
     BrowserModule
