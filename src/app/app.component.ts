b171034b517c780f6f8f106f0b325036d93be9e2
s8-dev <g.kuznetsov@siams.com>
2021-02-03 11:40:54 +0500

    pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,23 +7,7 @@ import { Component } from '@angular/core';
 })
 
 export class AppComponent {
-
-    arr = [1, 1, 3, 5, 8, 13]
-
-    objs = [
-        {title: 'Post1', author: "Vladilen", comments: [
-                {name: 'max', text: 'lorem 1'},
-                {name: 'max', text: 'lorem 2'},
-                {name: 'max', text: 'lorem 3'}
-            ]
-        },
-        {title: 'Post2', author: "Indrew", comments: [
-                {name: 'jack', text: 'lorem 1'},
-                {name: 'jack ', text: 'lorem 2'},
-                {name: 'jack', text: 'lorem 3'}
-            ]
-        }
-    ]
-
+    title = 'angular-basics'
+    now: Date = new Date()
 }
 
