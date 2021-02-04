4b2d0393f85f1ec4e764b15f02b2aeb80e7998f8
s8-dev <g.kuznetsov@siams.com>
2021-02-04 10:14:30 +0500

    Chapter-II: create post components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {PostComponent} from './post/post.component';
-import {Post2Component} from './post2/post2.component';
-import {Post3Component} from './post3/post3.component';
-import {Post4Component} from './post4/post4.component';
 import {FormsModule} from '@angular/forms';
+import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
     declarations: [
         AppComponent,
+        PostFormComponent,
+        PostComponent,
     ],
     imports: [
         BrowserModule,
