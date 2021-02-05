1fa655e2f4547958c2b3903942c796894958c7e8
s8-dev <g.kuznetsov@siams.com>
2021-02-05 14:58:21 +0500

    Chapter-III: prepare for directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-
-export interface Post {
-    title: string
-    text: string
-    id?:number
-}
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -12,34 +6,8 @@ export interface Post {
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent{
     title = 'angular-basics'
-
-    posts: Post[] = [
-        {title: 'I want to learn Angular components', text: 'I\'m steal learning components', id: 1},
-        {title: 'Next block', text: 'Directives & Pipes', id: 2},
-    ]
-
-    ngOnInit(): void {
-        setTimeout(() => {
-            console.log('Timeout')
-            this.posts[0] = {
-                title: 'changed',
-                text: 'changed2',
-                id: 33
-            }
-        }, 5000)
-    }
-
-    updatePosts(post: Post) {
-        this.posts.unshift(post)
-        console.log('Post', post)
-    }
-
-    removePost(id: number) {
-        console.log('Id to remove ', id)
-        this.posts = this.posts.filter(p => p.id !== id)
-    }
 
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,11 @@ import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
-import {PostComponent} from './post/post.component';
 import {FormsModule} from '@angular/forms';
-import { PostFormComponent } from './post-form/post-form.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        PostFormComponent,
-        PostComponent,
     ],
     imports: [
         BrowserModule,
