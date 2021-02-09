f92324f0d725c5fd30fdd1f0f892a2a34603e553
s8-dev <g.kuznetsov@siams.com>
2021-02-09 08:58:51 +0500

    Chapter-III: directives - add events

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,11 +1,25 @@
-import {Directive, ElementRef, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDirective {
     constructor(private el: ElementRef, private r: Renderer2) {
-        this.r.setStyle(el.nativeElement, 'color', 'blue')
+
+    }
+
+    @HostListener('click', ['$event.target']) onClick(evetn: Event) {
+        console.log(evetn)
+    }
+
+    @HostListener('mouseenter') onEnter() {
+        this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+        this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer')
+    }
+
+    @HostListener('mouseleave') onLeave() {
+        this.r.setStyle(this.el.nativeElement, 'color', null)
+        this.r.setStyle(this.el.nativeElement, 'cursor', null)
     }
 }
 
