9bdeb8cc8ce36038f65a7af4673625f4b85b533d
s8-dev <g.kuznetsov@siams.com>
2021-02-09 08:36:57 +0500

    Chapter-III: directive - get access to html element

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,8 +1,11 @@
-import {Directive} from '@angular/core';
+import {Directive, ElementRef, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDirective {
-
+    constructor(private el: ElementRef, private r: Renderer2) {
+        this.r.setStyle(el.nativeElement, 'color', 'blue')
+    }
 }
+
