c29214e14a7eaa4ff7618fb4c8fbf674cb54ad31
s8-dev <g.kuznetsov@siams.com>
2021-02-09 09:20:59 +0500

    Chapter-III: add parameters to directive way 2 - object

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -5,7 +5,7 @@ import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/co
 })
 export class StyleDirective {
     @Input('appStyle') color: string = 'blue'
-    @Input() fontWeight = 'normal'
+    @Input() dStyles: {border?: string, fontWeight?: string, borderRadius?: string}
 
     constructor(private el: ElementRef, private r: Renderer2) {
 
@@ -17,13 +17,17 @@ export class StyleDirective {
 
     @HostListener('mouseenter') onEnter() {
         this.r.setStyle(this.el.nativeElement, 'color', this.color)
-        this.r.setStyle(this.el.nativeElement, 'font-weight', this.fontWeight)
+        this.r.setStyle(this.el.nativeElement, 'font-weight', this.dStyles.fontWeight)
+        this.r.setStyle(this.el.nativeElement, 'border-radius', this.dStyles.borderRadius)
+        this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
         this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer')
     }
 
     @HostListener('mouseleave') onLeave() {
         this.r.setStyle(this.el.nativeElement, 'color', null)
         this.r.setStyle(this.el.nativeElement, 'font-weight', null)
+        this.r.setStyle(this.el.nativeElement, 'border-radius', null)
+        this.r.setStyle(this.el.nativeElement, 'border', null)
         this.r.setStyle(this.el.nativeElement, 'cursor', null)
     }
 }
