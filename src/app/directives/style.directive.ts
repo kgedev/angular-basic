e5b28883ef166bfb550a21bb015500624333385c
s8-dev <g.kuznetsov@siams.com>
2021-02-09 09:10:46 +0500

    Chapter-III: add parameters to directive way 1

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -1,9 +1,12 @@
-import {Directive, ElementRef, HostListener, Renderer2} from '@angular/core';
+import {Directive, ElementRef, HostListener, Input, Renderer2} from '@angular/core';
 
 @Directive({
     selector: '[appStyle]'
 })
 export class StyleDirective {
+    @Input('appStyle') color: string = 'blue'
+    @Input() fontWeight = 'normal'
+
     constructor(private el: ElementRef, private r: Renderer2) {
 
     }
@@ -13,12 +16,14 @@ export class StyleDirective {
     }
 
     @HostListener('mouseenter') onEnter() {
-        this.r.setStyle(this.el.nativeElement, 'color', 'blue')
+        this.r.setStyle(this.el.nativeElement, 'color', this.color)
+        this.r.setStyle(this.el.nativeElement, 'font-weight', this.fontWeight)
         this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer')
     }
 
     @HostListener('mouseleave') onLeave() {
         this.r.setStyle(this.el.nativeElement, 'color', null)
+        this.r.setStyle(this.el.nativeElement, 'font-weight', null)
         this.r.setStyle(this.el.nativeElement, 'cursor', null)
     }
 }
