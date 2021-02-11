a935b745c3c268578744ab64269ee565e6e2bf81
s8-dev <g.kuznetsov@siams.com>
2021-02-11 09:39:34 +0500

    Chapter-III: create structure directive

--- src/app/directives/style.directive.ts
+++ src/app/directives/style.directive.ts
@@ -9,9 +9,7 @@ export class StyleDirective {
 
     @HostBinding('style.color') elColor = null
 
-    constructor(private el: ElementRef, private r: Renderer2) {
-
-    }
+    constructor(private el: ElementRef, private r: Renderer2) {}
 
     @HostListener('click', ['$event.target']) onClick(evetn: Event) {
         console.log(evetn)
@@ -19,20 +17,10 @@ export class StyleDirective {
 
     @HostListener('mouseenter') onEnter() {
         this.elColor = this.color
-        // this.r.setStyle(this.el.nativeElement, 'color', this.color)
-        // this.r.setStyle(this.el.nativeElement, 'cursor', 'pointer')
-        // this.r.setStyle(this.el.nativeElement, 'font-weight', this.dStyles.fontWeight)
-        // this.r.setStyle(this.el.nativeElement, 'border-radius', this.dStyles.borderRadius)
-        // this.r.setStyle(this.el.nativeElement, 'border', this.dStyles.border)
     }
 
     @HostListener('mouseleave') onLeave() {
         this.elColor = null
-        // this.r.setStyle(this.el.nativeElement, 'color', null)
-        // this.r.setStyle(this.el.nativeElement, 'cursor', null)
-        // this.r.setStyle(this.el.nativeElement, 'font-weight', null)
-        // this.r.setStyle(this.el.nativeElement, 'border-radius', null)
-        // this.r.setStyle(this.el.nativeElement, 'border', null)
     }
 }
 
