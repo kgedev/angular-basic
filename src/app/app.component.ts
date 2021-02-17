197331529dd7b6bfeeb2aa601c420d3c77c16de8
s8-dev <g.kuznetsov@siams.com>
2021-02-17 10:57:36 +0500

    Chapter-IV: create dynamic filtration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,10 @@
 import {Component} from '@angular/core';
 
+export interface Post {
+    title: string
+    text: string
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +12,13 @@ import {Component} from '@angular/core';
 })
 
 export class AppComponent{
+
+    search = ''
+
+    posts: Post[] = [
+        {title: 'Beer', text: 'The best beer in the world'},
+        {title: 'Bread', text: 'The best bread in the world'},
+        {title: 'TypeScript', text: 'The best language in the world'},
+    ]
 }
 
