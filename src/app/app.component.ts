187c09c0379785c5b6e6b2b980fbfea9a32cb61c
s8-dev <g.kuznetsov@siams.com>
2021-02-19 08:37:35 +0500

    Chapter-V: services - prepare for services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,13 @@
 import {Component, OnInit} from '@angular/core';
 import {Observable} from 'rxjs';
 
-export interface Post {
-    title: string
-    text: string
-}
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit{
+export class AppComponent {
 
-    p: Promise<string> = new Promise<string>(resolve => {
-        setTimeout(() => {
-            resolve('Promise Resolved')
-        }, 4000)
-    })
-
-    date$: Observable<Date> = new Observable<Date>(obs => {
-        setInterval(() => {
-            obs.next(new Date())
-        }, 1000)
-    })
-
-    date: Date
-
-    ngOnInit(): void {
-        this.date$.subscribe( date => {
-            this.date = date
-        })
-    }
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,11 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import {MultByPipe} from './pipes/mult-by.pipe';
-import { ExMarksPipe } from './pipes/ex-marks.pipe';
-import { FilterPipe } from './pipes/filter.pipe';
 
 @NgModule({
     declarations: [
         AppComponent,
-        MultByPipe,
-        ExMarksPipe,
-        FilterPipe,
+
     ],
     imports: [
         BrowserModule,
