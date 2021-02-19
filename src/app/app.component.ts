b85aec54723e8098398abcd23dd5406ea832c665
s8-dev <g.kuznetsov@siams.com>
2021-02-19 09:42:22 +0500

    Chapter-V: add local service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 import {AppCounterService} from './app-counter.services.ts/AppCounterService';
+import {LocalCounterService} from './services/local-counter.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss'],
+    providers: [LocalCounterService]
 })
 
 export class AppComponent {
-    constructor(public appCounterService: AppCounterService) {
-    }
+    constructor(
+        public appCounterService: AppCounterService,
+        public localCounterService: LocalCounterService
+    ) {}
 }
 
