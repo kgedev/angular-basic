5ff9f2508eef42bd7265a7e6d679da663bc42764
glebus <svarog96@gmail.com>
2021-02-24 09:58:01 +0500

    Chapter-VI: Angular Forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import {Component, OnInit} from '@angular/core';
-import {AppCounterService} from './app-counter.services.ts/AppCounterService';
-import {LocalCounterService} from './services/local-counter.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.scss'],
-    providers: [LocalCounterService]
+    styleUrls: ['./app.component.scss']
 })
 
 export class AppComponent {
-    constructor(
-        public appCounterService: AppCounterService,
-        public localCounterService: LocalCounterService
-    ) {}
+    constructor() {}
 }
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule} from '@angular/forms';
-import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        CounterComponent,
     ],
     imports: [
         BrowserModule,
