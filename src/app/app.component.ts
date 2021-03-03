648a55c31424c6b5fb3a765593445d840e0875c7
s8-dev <g.kuznetsov@siams.com>
2021-03-03 11:08:24 +0500

    Chapter-VI: preparing for custom ngModel creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,4 @@
-import {Component, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
-import {MyValidators} from './my.validators';
+import {Component} from '@angular/core';
 
 @Component({
     selector: 'app-root',
@@ -8,56 +6,8 @@ import {MyValidators} from './my.validators';
     styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit {
-    form: FormGroup;
+export class AppComponent {
 
-    ngOnInit() {
-        this.form = new FormGroup({
-            email: new FormControl('', [
-                Validators.email,
-                Validators.required,
-                MyValidators.restrictedEmails
-            ], [MyValidators.uniqEmail]),
-            password: new FormControl(null, [
-                Validators.required,
-                Validators.minLength(6)
-            ]),
-            address: new FormGroup({
-                country: new FormControl('ru'),
-                city: new FormControl('', Validators.required)
-            }),
-            skills: new FormArray([])
-        });
-    }
-
-    submit() {
-        if (this.form.valid) {
-            console.log('Form: ', this.form);
-            const formData = {...this.form.value};
-            console.log('Form Data', formData);
-
-            this.form.reset()
-        }
-    }
-
-    setCapital() {
-        const cityMap = {
-            ru: 'Москва',
-            ua: 'Киев',
-            by: 'Минск'
-        }
-
-        const cityKey = this.form.get('address').get('country').value
-        const city = cityMap[cityKey]
-
-        this.form.patchValue({address: {city}})
-    }
-
-    addSkill() {
-        const control = new FormControl('', Validators.required);
-        // (<FormArray>this.form.get('skills')).push(control)
-        (this.form.get('skills') as FormArray).push(control)
-    }
 }
 
 
