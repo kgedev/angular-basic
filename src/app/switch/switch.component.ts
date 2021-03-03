a4d22fae78f57f14d87056222ac6621f4182f6fd
s8-dev <g.kuznetsov@siams.com>
2021-03-03 11:18:25 +0500

    Chapter-VI: ngModel add structure

---
+++ src/app/switch/switch.component.ts
@@ -0,0 +1,22 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    selector: 'app-switch',
+    templateUrl: './switch.component.html',
+    styleUrls: ['./switch.component.scss']
+})
+export class SwitchComponent implements OnInit {
+
+    state = 'off'
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+    }
+
+    setState(state: string) {
+        this.state = state
+    }
+
+}
