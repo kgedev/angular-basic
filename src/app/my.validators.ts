ac79f2b46ba0277d576c351987ca669a9d290358
s8-dev <g.kuznetsov@siams.com>
2021-03-03 10:56:06 +0500

    Chapter-VI: add async validators

--- src/app/my.validators.ts
+++ src/app/my.validators.ts
@@ -1,4 +1,5 @@
 import {FormControl} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 export class MyValidators {
 
@@ -11,4 +12,15 @@ export class MyValidators {
         return null
     }
 
+    static uniqEmail(control: FormControl): Promise<any> | Observable<any> {
+        return new Promise(resolve => {
+            setTimeout(() => {
+                if(control.value === 'async@mail.ru') {
+                    resolve({uniqEmail: true})
+                } else {
+                    resolve(null)
+                }
+            }, 1000)
+        })
+    }
 }
