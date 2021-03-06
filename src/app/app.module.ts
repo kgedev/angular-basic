e72aa26e7441bef04f01408cb7a7bf514270a745
glebus <svarog96@gmail.com>
2021-03-06 10:47:14 +0500

    Chapter-VII: create first http query

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
     declarations: [
@@ -11,6 +12,7 @@ import {FormsModule, ReactiveFormsModule} from '@angular/forms';
     imports: [
         BrowserModule,
         FormsModule,
+        HttpClientModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
