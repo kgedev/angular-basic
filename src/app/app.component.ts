da7d561c66081f097537fa5723655cbaf63b884d
glebus <svarog96@gmail.com>
2021-03-06 11:55:01 +0500

    Chapter-VII: create post query

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,19 +14,38 @@ export interface Todo {
 })
 
 export class AppComponent implements OnInit{
-
+    url = 'https://jsonplaceholder.typicode.com/todos'
     todos: Todo[] = []
+
+    todoTitle = ''
 
     constructor(private http: HttpClient) {
     }
     ngOnInit() {
-        this.http.get<Todo[]>('https://jsonplaceholder.typicode.com/todos?_limit=5')
+        this.http.get<Todo[]>(this.url + '?_limit=2')
             .subscribe(todos => {
                 console.log('Response: ',todos)
                 this.todos = todos
             })
     }
 
+    addTodo() {
+        if(!this.todoTitle.trim()) {
+            return
+        }
+
+        const newTodo: Todo = {
+            title: this.todoTitle,
+            completed: false
+        }
+
+        this.http.post<Todo>(this.url, newTodo)
+            .subscribe(todo => {
+                console.log('todo: ', todo)
+                this.todos.push(todo)
+                this.todoTitle = ''
+            })
+    }
 }
 
 
