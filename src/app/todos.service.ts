b2e27b7399653cc5cd297c7c9480d7332271c06f
s8-dev <g.kuznetsov@siams.com>
2021-03-13 09:23:41 +0500

    Chapter-VII: add headers

--- src/app/todos.service.ts
+++ src/app/todos.service.ts
@@ -1,5 +1,5 @@
 import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable, throwError} from 'rxjs';
 import {catchError, delay} from 'rxjs/operators';
 
@@ -11,33 +11,38 @@ export interface Todo {
 
 @Injectable({providedIn: 'root'})
 export class TodosService {
-    private url = 'https://jsonplaceholder.typicode.com/todos'
+    private url = 'https://jsonplaceholder.typicode.com/todos';
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) {
+    }
 
     addTodo(todo: Todo): Observable<Todo> {
-         return this.http.post<Todo>(this.url, todo)
+        const headers = new HttpHeaders({
+            'MyCustomHeader': Math.random().toString(),
+            'OnlyStringValue': '12345'
+        })
+
+        return this.http.post<Todo>(this.url, todo, {headers});
     }
 
     fetchTodos(): Observable<Todo[]> {
-        return this.http.get<Todo[]>(this.url + '9?_limit=2')
+        return this.http.get<Todo[]>(this.url + '?_limit=2')
             .pipe(
                 delay(500),
                 catchError(error => {
-                    console.log('Error: ', error.message)
-                    return throwError(error)
+                    console.log('Error: ', error.message);
+                    return throwError(error);
                 })
-
-            )
+            );
     }
 
     removeTodo(id: number): Observable<void> {
-        return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        return this.http.delete<void>(`https://jsonplaceholder.typicode.com/todos/${id}`);
     }
 
     completeTodo(id: number): Observable<Todo> {
         return this.http.put<Todo>(`${this.url}/${id}`, {
             completed: true
-        })
+        });
     }
 }
