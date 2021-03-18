4a73ded2140166fdaaf691644aa7b091b1ce1042
s8-dev <g.kuznetsov@siams.com>
2021-03-18 10:08:25 +0500

    Chapter-VIII: add Guard for nested page

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -1,16 +1,19 @@
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, Router, RouterStateSnapshot} from '@angular/router';
 import {Observable} from 'rxjs';
 import {Injectable} from '@angular/core';
 import {AuthService} from './auth.service';
 
 @Injectable({providedIn: 'root'})
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanActivateChild {
 
     constructor(
         private authService: AuthService,
         private router: Router
     ) {}
 
+    canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean{
+        return this.canActivate(childRoute, state)
+    }
 
     canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean{
         return this.authService.isAuthenticated().then(isAuth => {
