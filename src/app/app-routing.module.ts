5eec0e330cdcd19efa7f606eae0316e255a107b3
glebus <svarog96@gmail.com>
2021-03-21 12:08:38 +0500

    Chapter-IX: add lazy-loading for about page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,11 @@ import {RouterModule} from '@angular/router';
 import {HomePageComponent} from './home-page/home-page.component';
 
 @NgModule({
-  imports: [RouterModule.forRoot([
-    {path: '', component: HomePageComponent, pathMatch: 'full'}
-  ])],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot([
+        {path: '', component: HomePageComponent, pathMatch: 'full'},
+        {path: 'about', loadChildren: './about-page/about-page.module#AboutPageModule'}
+    ])],
+    exports: [RouterModule]
 })
 export class AppRoutingModule {
 
