c37508d8b2a3a0c302ae718c3cf764e955b8b03a
s8-dev <g.kuznetsov@siams.com>
2021-03-26 09:05:17 +0500

    Chapter-XI: arrays and strings tests

---
+++ src/app/playground/countries.spec.ts
@@ -0,0 +1,12 @@
+import {countries} from './countries'
+
+describe('countries', () => {
+
+  it('should contain countries code', () => {
+    const result = countries()
+    expect(result).toContain('RU')
+    expect(result).toContain('UA')
+    expect(result).toContain('BY')
+  })
+
+})
