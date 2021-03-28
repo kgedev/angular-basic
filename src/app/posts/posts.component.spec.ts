604a72e3ffce1a764dc81796529d9eb1f72b26dc
glebus <svarog96@gmail.com>
2021-03-28 10:51:19 +0500

    Chapter-XI: add tests for ngOnInit & fetch()

--- src/app/posts/posts.component.spec.ts
+++ src/app/posts/posts.component.spec.ts
@@ -1,7 +1,28 @@
+import {PostsComponent} from './posts.component';
+import {PostsService} from './posts.service';
+import {EMPTY, of} from 'rxjs';
+
 describe('PostsComponent', () => {
+  let component : PostsComponent
+  let service: PostsService
 
-  it('', () => {
-
+  beforeEach(() => {
+    service = new PostsService(null)
+    component = new PostsComponent(service)
   })
 
+  it('should call fetch when ngOnInit', () => {
+    const spy = spyOn(service, 'fetch').and.callFake(() => {
+      return EMPTY
+    })
+    component.ngOnInit()
+    expect(spy).toHaveBeenCalled()
+  })
+
+  it('should update posts length after ngOnInit', () => {
+    const posts = [1, 2, 3, 4]
+    spyOn(service, 'fetch').and.returnValue(of(posts))
+    component.ngOnInit()
+    expect(component.posts.length).toBe(posts.length)
+  })
 })
