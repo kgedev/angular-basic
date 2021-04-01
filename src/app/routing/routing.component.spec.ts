35a8242e905b3f51fe855db9560b3bf63b6983de
s8-dev <g.kuznetsov@siams.com>
2021-04-01 09:44:39 +0500

    Chapter-XII: prepare for Routing test

---
+++ src/app/routing/routing.component.spec.ts
@@ -0,0 +1,22 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { RoutingComponent } from './routing.component';
+
+describe('RoutingComponent', () => {
+  let component: RoutingComponent;
+  let fixture: ComponentFixture<RoutingComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [ RoutingComponent ]
+    })
+
+    fixture = TestBed.createComponent(RoutingComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should be defined', () => {
+    expect(component).toBeDefined();
+  });
+});
