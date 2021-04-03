1068060195c8e5f351031f46b4a6411ae501e06a
glebus <svarog96@gmail.com>
2021-04-03 17:16:17 +0500

    Chapter-XII: Router test

--- src/app/routing/routing.component.spec.ts
+++ src/app/routing/routing.component.spec.ts
@@ -1,6 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RoutingComponent } from './routing.component';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Observable} from 'rxjs';
+
+class RouterStub {
+  navigate(path: String[]) {}
+}
+
+class ActivatedRouteStub {
+  params: Observable<Params>
+}
 
 describe('RoutingComponent', () => {
   let component: RoutingComponent;
@@ -8,15 +17,27 @@ describe('RoutingComponent', () => {
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [ RoutingComponent ]
+      declarations: [ RoutingComponent ],
+      providers: [
+        {provide: Router, useClass: RouterStub},
+        {provide: ActivatedRoute, useClass: ActivatedRouteStub}
+      ]
     })
 
     fixture = TestBed.createComponent(RoutingComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should be defined', () => {
     expect(component).toBeDefined();
+  });
+
+  it('should navigate to posts if go back', () =>  {
+    let router = TestBed.inject(Router)
+    let spy = spyOn(router, 'navigate')
+
+    component.goBack()
+
+    expect(spy).toHaveBeenCalledWith(['/posts'])
   });
 });
